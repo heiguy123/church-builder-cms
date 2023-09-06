01a4cbd722fcb23286a546859daef6d486089f5b
Moses Lau Yi Hieng <65391344+heiguy123@users.noreply.github.com>
2023-09-06 15:50:36 +0800

    home add link

--- web-cms/src/app/home/home.component.ts
+++ web-cms/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor (
+    private router: Router,
+  ) { }
+
+  goToRegister() : void {
+    this.router.navigate(['/register']);
+  }
+
+  goToLogin() : void {
+    this.router.navigate(['/login']);
+  }
 }
