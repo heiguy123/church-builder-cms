6ba7f644aa5388046702ab29643108e8dd011e7d
Moses Lau Yi Hieng <65391344+heiguy123@users.noreply.github.com>
2023-09-13 15:54:04 +0800

    fetch data to display single detail

--- web-cms/src/app/master-admin/dashboard/pages/single-user-request/single-user-request.component.ts
+++ web-cms/src/app/master-admin/dashboard/pages/single-user-request/single-user-request.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UserRequest } from '../../models/user-request';
+import { doc, getDoc, getFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-single-user-request',
@@ -8,10 +10,40 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./single-user-request.component.scss']
 })
 export class SingleUserRequestComponent implements OnInit {
+  table: UserRequest[] = [];
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id'); // Use the correct parameter name
-    
+    const userId = this.route.snapshot.paramMap.get('id'); // Use the correct parameter name
+    this.fectchUserRequest(userId!);
+  }
+
+  async fectchUserRequest(userId: string) {
+    const firestore = getFirestore();
+    const userID = userId;
+    const docRef = doc(firestore, 'users', userID);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      this.table = [
+        {
+          request_id: userId,
+          creation_date: docSnap.data()['docCreatedDate'],
+          organization_name: docSnap.data()['orgName'],
+          applicant_email: docSnap.data()['email'],
+          document_link: docSnap.data()['docUrl'],
+          activated: docSnap.data()['activated'],
+          applicant_first_name: docSnap.data()['firstName'],
+          applicant_last_name: docSnap.data()['lastName'],
+          organization_address: docSnap.data()['orgAddress'],
+          organization_city: docSnap.data()['orgCity'],
+          organizatiton_demonination: docSnap.data()['orgDemonination'],
+          organization_member_size: docSnap.data()['orgMemberSize'],
+          organization_state: docSnap.data()['orgState'],
+          organization_zip: docSnap.data()['orgZip'],
+          role: docSnap.data()['role']
+        },
+      ];
+    }
   }
 }
