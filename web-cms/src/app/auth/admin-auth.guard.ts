3e05fe825b996eb4f8ab9581996ef33d974e811b
Moses Lau Yi Hieng <65391344+heiguy123@users.noreply.github.com>
2023-09-21 03:33:57 +0800

    implement guard service for 3 roles

--- web-cms/src/app/auth/admin-auth.guard.ts
+++ web-cms/src/app/auth/admin-auth.guard.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminAuthGuard implements CanActivate {
+  constructor(
+    private cookieService: CookieService,
+    private router: Router,
+    private toastr: ToastrService,  
+  ) {}
+
   canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean | UrlTree {
+    const cookieExists: boolean = this.cookieService.check('role');
+    const cookieValue: string = this.cookieService.get('role');
+    if (cookieExists && cookieValue === 'admin') {
+      return true;
+    } else {
+      this.toastr.error("You are not authorized to access the page", 'Error', {
+        timeOut: 3000,
+        progressBar: true,
+        progressAnimation: 'increasing',
+        positionClass: 'toast-top-right',
+      });
+      if (cookieExists) {
+        this.cookieService.deleteAll();
+      }
+      return this.router.parseUrl('/login');
+    }
   }
-  
 }
