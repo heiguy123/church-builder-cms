6e97dc5175d25b045c518ca0d7257088a570c1fa
Moses Lau Yi Hieng <65391344+heiguy123@users.noreply.github.com>
2023-09-20 23:25:28 +0800

    super admin dashboard logic + remove unnecesities

--- web-cms/src/app/super-admin/dashboard/dashboard-routing.module.ts
+++ web-cms/src/app/super-admin/dashboard/dashboard-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
-import { UserRequestComponent } from './pages/user-request/user-request.component';
-import { SingleUserRequestComponent } from './pages/single-user-request/single-user-request.component';
 import { WorkspaceComponent } from './pages/workspace/workspace.component';
 
 const routes: Routes = [
@@ -10,8 +8,6 @@ const routes: Routes = [
     path: '',
     component: DashboardComponent,
     children: [
-      { path: 'app-user-request', component: UserRequestComponent, },
-      { path: 'app-user-request/details/:id', component: SingleUserRequestComponent },
       { path: 'app-workspace', component: WorkspaceComponent },
     ],
   },
--- web-cms/src/app/super-admin/dashboard/dashboard.module.ts
+++ web-cms/src/app/super-admin/dashboard/dashboard.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { RequestTableComponent } from './components/request-table/request-table.component';
-import { RequestTableItemComponent } from './components/request-table-item/request-table-item.component';
-import { SingleUserRequestComponent } from './pages/single-user-request/single-user-request.component';
 import { WorkspaceComponent } from './pages/workspace/workspace.component';
-import { WorkspaceTableComponent } from './components/workspace-table/workspace-table.component';
-import { WorkspaceTableItemComponent } from './components/workspace-table-item/workspace-table-item.component';
 
 @NgModule({
-  imports: [DashboardRoutingModule, RequestTableComponent,
-    RequestTableItemComponent, SingleUserRequestComponent, WorkspaceComponent, WorkspaceTableComponent,
-    WorkspaceTableItemComponent],
+  imports: [DashboardRoutingModule, WorkspaceComponent],
 })
 export class DashboardModule {}
